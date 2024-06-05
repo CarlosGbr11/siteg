8671f012baae4b49a7ad29ee3edebf1fad53e2f7
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 02:42:16 -0300

    Create ComprarTenis3.jsx

---
+++ frontend/src/pages/ComprarTenis3.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ComprarTenis1 = () => {
+    return (
+        <div>
+            <h3>AIR FORCE 1 ´07 LV8</h3>
+            <p>R$ 679,99</p>
+            <p>Adicionar ao Carrinho</p>
+        </div>
+    );
+}
+
+export default ComprarTenis1;
