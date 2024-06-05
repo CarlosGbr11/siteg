04afd88aa0a90930c9dd96130de004b6ee17714f
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 02:56:42 -0300

    Update ComprarTenis1.jsx

--- frontend/src/pages/ComprarTenis1.jsx
+++ frontend/src/pages/ComprarTenis1.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import tenis1 from '../images/tenis1.jpg'; 
 
 const ComprarTenis1 = () => {
     return (
         <div>
-            <h3>DUNK LOW OG</h3>
-            <p>R$ 589,99</p>
+            <img src={tenis1} alt="Tênis 1" />
+            <h3>DUNK LOW RETRO</h3>
+            <p>R$ 639,99</p>
             <p>Adicionar ao Carrinho</p>
         </div>
     );
