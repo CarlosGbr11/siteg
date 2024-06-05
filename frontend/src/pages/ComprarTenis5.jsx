944eefe0ba3206bf6733931b86badf3f6e877974
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 02:42:19 -0300

    Create ComprarTenis5.jsx

---
+++ frontend/src/pages/ComprarTenis5.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ComprarTenis1 = () => {
+    return (
+        <div>
+            <h3>STAN SMITH</h3>
+            <p>R$ 339,99</p>
+            <p>Adicionar ao Carrinho</p>
+        </div>
+    );
+}
+
+export default ComprarTenis1;
