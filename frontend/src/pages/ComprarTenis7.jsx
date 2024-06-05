de8f860e7c9b5af1cd7fd032ac7f93a489d680fe
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 02:42:22 -0300

    Create ComprarTenis7.jsx

---
+++ frontend/src/pages/ComprarTenis7.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ComprarTenis1 = () => {
+    return (
+        <div>
+            <h3>HANDBALL SPEZIAL</h3>
+            <p>R$ 399,99</p>
+            <p>Adicionar ao Carrinho</p>
+        </div>
+    );
+}
+
+export default ComprarTenis1;
