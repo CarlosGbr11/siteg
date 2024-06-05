e9e3c14f014acd10887a40de4d6a84d363ae9715
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 02:42:09 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,15 @@ import Header from './pages/Header';
 import Inicio from './components/Inicio';
 import Nike from './components/Nike';
 import Adidas from './components/Adidas';
+import ComprarTenis1 from './pages/ComprarTenis1';
+import ComprarTenis2 from './pages/ComprarTenis2';
+import ComprarTenis3 from './pages/ComprarTenis3';
+import ComprarTenis4 from './pages/ComprarTenis4';
+import ComprarTenis5 from './pages/ComprarTenis5';
+import ComprarTenis6 from './pages/ComprarTenis6';
+import ComprarTenis7 from './pages/ComprarTenis7';
+import ComprarTenis8 from './pages/ComprarTenis8';
+import ComprarTenis9 from './pages/ComprarTenis9';
 import './App.css'
 
 function App() {
@@ -13,16 +22,26 @@ function App() {
     <Router>
 
       <div className='container-app'>
-        
-          <Header />
+
+        <Header />
         <main>
           <Routes>
             <Route path='/' element={<Inicio />} />
             <Route path='/Nike' element={<Nike />} />
             <Route path='/Adidas' element={<Adidas />} />
+            <Route path="/comprar/tenis1" element={<ComprarTenis1 />} />
+            <Route path="/comprar/tenis2" element={<ComprarTenis2 />} />
+            <Route path="/comprar/tenis3" element={<ComprarTenis3 />} />
+            <Route path="/comprar/tenis4" element={<ComprarTenis4 />} />
+            <Route path="/comprar/tenis5" element={<ComprarTenis5 />} />
+            <Route path="/comprar/tenis6" element={<ComprarTenis6 />} />
+            <Route path="/comprar/tenis7" element={<ComprarTenis7 />} />
+            <Route path="/comprar/tenis8" element={<ComprarTenis8 />} />
+            <Route path="/comprar/tenis9" element={<ComprarTenis9 />} />
           </Routes>
+
         </main>
-          <Footer />
+        <Footer />
       </div>
 
     </Router>
