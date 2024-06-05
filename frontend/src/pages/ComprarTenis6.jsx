5fc2d9d3fa3be0a9308772708473dcdc9717e8f6
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 02:42:20 -0300

    Create ComprarTenis6.jsx

---
+++ frontend/src/pages/ComprarTenis6.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ComprarTenis1 = () => {
+    return (
+        <div>
+            <h3>FORUM 84 LOW</h3>
+            <p>R$ 579,99</p>
+            <p>Adicionar ao Carrinho</p>
+        </div>
+    );
+}
+
+export default ComprarTenis1;
