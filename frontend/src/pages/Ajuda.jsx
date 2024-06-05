95ed34f515d1c888e6e16b7c5a7f7e8b0793e81e
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 15:24:35 -0300

    Update Ajuda.jsx

--- frontend/src/pages/Ajuda.jsx
+++ frontend/src/pages/Ajuda.jsx
@@ -8,11 +8,24 @@ import visa from '../images/visa.png';
 const Ajuda = () => {
     return (
         <div className="suporte-container">
-            <h2>Nosso Atendimento</h2>
+            <h2>Como comentar</h2>
             <p>
-                Na Hype, estamos comprometidos em proporcionar uma experiência de compra excepcional para nossos clientes. Entendemos que um atendimento de qualidade é essencial para garantir a sua satisfação. Por isso, nossa equipe está sempre pronta para ajudar você em todas as etapas do processo de compra, desde a escolha do produto até o pós-venda.
+                <strong>Localize o Formulário de Comentários:</strong>
+                Em cada página de produto ou de avaliação, haverá um formulário de comentários onde os usuários podem deixar suas opiniões. Esse formulário geralmente estará localizado abaixo das avaliações existentes.
             </p>
-
+            <p>
+                <strong>Clique no Campo de Comentário:</strong>
+                Clique no campo de texto designado para deixar um comentário. Ele geralmente estará marcado com algo como "Deixe seu comentário aqui".
+            </p>
+            <p>
+                <strong>Digite seu Comentário:</strong>
+                Escreva seu comentário no campo de texto. Você pode compartilhar sua opinião sobre o calçado, detalhes sobre o ajuste, conforto, estilo, ou qualquer outra informação relevante que desejar compartilhar.
+            </p>
+            <p>
+                <strong>Enviar o Comentário:</strong>
+                Após escrever seu comentário e, se aplicável, selecionar uma classificação, clique no botão "Enviar", "Postar" ou similar para enviar seu comentário.
+            </p>
+            
             <h2>Formas de pagamento</h2>
             <div className="formas-pagamento">
                 <img src={elo} alt="elo" />
