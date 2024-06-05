feb179fe89a4115db458f555a5023ff3b86049d0
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 02:42:14 -0300

    Create ComprarTenis2.jsx

---
+++ frontend/src/pages/ComprarTenis2.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ComprarTenis1 = () => {
+    return (
+        <div>
+            <h3>DUNK LOW RETRO</h3>
+            <p>R$ 639,99</p>
+            <p>Adicionar ao Carrinho</p>
+        </div>
+    );
+}
+
+export default ComprarTenis1;
