af75b6603d743f9f73f5b3ed830b43898348efff
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 03:40:37 -0300

    Update ComprarTenis2.jsx

--- frontend/src/pages/ComprarTenis2.jsx
+++ frontend/src/pages/ComprarTenis2.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import tenis2 from '../images/tenis2.jpg'; 
+import './ComprarTenis.css'; 
 
 const ComprarTenis2 = () => {
     return (
-        <div>
-            <img src={tenis2} alt="Tênis 2" />
-            <h3>DUNK LOW OG</h3>
-            <p>R$ 589,99</p>
-            <p>Adicionar ao Carrinho</p>
+        <div className="comprar-container">
+            <img src={tenis2} alt="Tênis 2" className="tenis-img"/>
+            <h3 className="tenis-nome">DUNK LOW OG</h3>
+            <p className="tenis-preco">R$ 589,99</p>
+            <button className="btn-comprar">Adicionar ao Carrinho</button>
         </div>
     );
 }
