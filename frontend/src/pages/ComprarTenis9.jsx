74bcb585033ec36db190c6ea61ddd35c6109d81f
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 02:42:25 -0300

    Create ComprarTenis9.jsx

---
+++ frontend/src/pages/ComprarTenis9.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ComprarTenis1 = () => {
+    return (
+        <div>
+            <h3>SUPERSTAR XLG</h3>
+            <p>R$ 399,99</p>
+            <p>Adicionar ao Carrinho</p>
+        </div>
+    );
+}
+
+export default ComprarTenis1;
