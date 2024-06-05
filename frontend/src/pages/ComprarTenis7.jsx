457b69dd8a9b5580291f9808ec566e955312c277
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 15:53:39 -0300

    Update ComprarTenis7.jsx

--- frontend/src/pages/ComprarTenis7.jsx
+++ frontend/src/pages/ComprarTenis7.jsx
@@ -9,7 +9,7 @@ const ComprarTenis7 = () => {
             <img src={tenis7} alt="Tênis 7" className="tenis-img"/>
             <h3 className="tenis-nome">HANDBALL SPEZIAL</h3>
             <p className="tenis-preco">R$ 399,99</p>
-            <button className="btn-comprar">Adicionar ao Carrinho</button>
+            <button className="btn-comprar">Adicionar comentário</button>
         </div>
     );
 }
