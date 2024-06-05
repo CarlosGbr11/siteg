f3fd29b93ec29517a618e363d3531557e2264426
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 03:40:38 -0300

    Update ComprarTenis3.jsx

--- frontend/src/pages/ComprarTenis3.jsx
+++ frontend/src/pages/ComprarTenis3.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import tenis3 from '../images/tenis3.jpg'; 
+import './ComprarTenis.css'; 
 
 
 const ComprarTenis3 = () => {
     return (
-        <div>
-            <img src={tenis3} alt="Tênis 3" />
-            <h3>AIR FORCE 1 ´07 LV8</h3>
-            <p>R$ 679,99</p>
-            <p>Adicionar ao Carrinho</p>
+        <div className="comprar-container">
+            <img src={tenis3} alt="Tênis 3" className="tenis-img"/>
+            <h3 className="tenis-nome">AIR FORCE 1 ´07 LV8</h3>
+            <p className="tenis-preco">R$ 679,99</p>
+            <button className="btn-comprar">Adicionar ao Carrinho</button>
         </div>
     );
 }
