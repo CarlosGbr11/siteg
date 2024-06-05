77852c50aa71a6aebb130c3ce6067a475f4325b0
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 13:58:54 -0300

    Update Suporte.jsx

--- frontend/src/pages/Suporte.jsx
+++ frontend/src/pages/Suporte.jsx
@@ -1,10 +1,55 @@
 import React from "react";
+import './Suporte.css';
 
-const Suporte = () =>{
-    return(
-        <div>
-            <p>Suporte</p>
-        </div>
-    )
+const Suporte = () => {
+    return (
+        <div className="suporte-container">
+            <h2>Regras de Entrega</h2>
+            <p>
+                As entregas são realizadas de segunda a sexta, das 9h às 18h, exceto feriados.
+            </p>
+            <p>
+                Toda entrega deverá ter o comprovante assinado pelo recebedor, com preenchimento de data, nome legível e número do RG.
+            </p>
+            <p>
+                <strong>Entregas via Correios:</strong> Locais de destino com baixa população, de difícil acesso ou distantes das capitais podem necessitar de mais prazo para a entrega ou serem retirados na agência dos Correios mais próxima.
+            </p>
+            <p>
+                Locais de destino sujeitos a prazo diferenciado pelos Correios podem ter um acréscimo de até 4 dias úteis para a entrega ou necessitar que os itens sejam retirados na agência dos Correios mais próxima.
+            </p>
+            <p>
+                Para consultar se o CEP de destino tem restrição de entrega, <a href="https://www.correios.com.br" target="_blank" rel="noopener noreferrer">clique aqui</a> e consulte no site dos Correios. Preencha o CEP de Origem 06833-073, o CEP de Destino da entrega e o Tipo de Serviço.
+            </p>
+            <p>
+                As entregas são realizadas apenas dentro do território nacional.
+            </p>
+            <p>
+                Não haverá reembolso do frete ao cliente em caso de uma segunda troca dos mesmos produtos ou pedido.
+            </p>
+            <p>
+                Em caso de dúvidas ou esclarecimentos, por favor, contate nosso serviço de  Atendimento ao Cliente.
+            </p>
+
+            <h2>Como acompanhar meu pedido?</h2>
+            <p>
+                Ao realizar um pedido em nossa loja online você receberá comunicações diretamente em seu e-mail cadastrado a cada nova etapa avançada, até o momento da entrega.
+            </p>
+            <p>
+                <strong>Eles são separados por categorias:</strong>
+            </p>
+            <p>
+                <strong>Pedido realizado:</strong> Significa que acabamos de receber o seu pedido e estamos analisando as informações;
+            </p>
+            <p>
+                <strong>Status do pagamento:</strong> Nessa etapa será informado se seu pagamento foi concluído ou não. Confira aqui mais detalhes sobre prazos para aprovação;
+            </p>
+            <p>
+                <strong>Entregue para transportadora:</strong> O pedido está pronto para ser enviado para o endereço cadastrado;
+                Pedido entregue: Notificação de que o pacote foi entregue.
+            </p>
+
+        </div >
+    );
 }
+
 export default Suporte;
