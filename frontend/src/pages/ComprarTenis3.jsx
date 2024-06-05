50840e2039b07027648237c9c488c00dd9fbbaad
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 02:56:45 -0300

    Update ComprarTenis3.jsx

--- frontend/src/pages/ComprarTenis3.jsx
+++ frontend/src/pages/ComprarTenis3.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import tenis3 from '../images/tenis3.jpg'; 
 
-const ComprarTenis1 = () => {
+
+const ComprarTenis3 = () => {
     return (
         <div>
+            <img src={tenis3} alt="Tênis 3" />
             <h3>AIR FORCE 1 ´07 LV8</h3>
             <p>R$ 679,99</p>
             <p>Adicionar ao Carrinho</p>
@@ -10,4 +13,4 @@ const ComprarTenis1 = () => {
     );
 }
 
-export default ComprarTenis1;
+export default ComprarTenis3;
