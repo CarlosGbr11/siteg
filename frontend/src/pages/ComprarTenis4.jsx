9f9f7137a225992c2bbde70e857d858dfa50b627
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 15:53:31 -0300

    Update ComprarTenis4.jsx

--- frontend/src/pages/ComprarTenis4.jsx
+++ frontend/src/pages/ComprarTenis4.jsx
@@ -9,7 +9,7 @@ const ComprarTenis4 = () => {
             <img src={tenis4} alt="Tênis 4" className="tenis-img"/>
             <h3 className="tenis-nome">JORDAN 1 MID</h3>
             <p className="tenis-preco">R$ 839,99</p>
-            <button className="btn-comprar">Adicionar ao Carrinho</button>
+            <button className="btn-comprar">Adicionar comentário</button>
         </div>
     );
 }
