683c721ca71dc9e8e51cda795c7fc7729ab096b3
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 03:40:42 -0300

    Update ComprarTenis6.jsx

--- frontend/src/pages/ComprarTenis6.jsx
+++ frontend/src/pages/ComprarTenis6.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import tenis6 from '../images/tenis6.jpg'; 
+import './ComprarTenis.css'; 
 
 
 const ComprarTenis6 = () => {
     return (
-        <div>
-            <img src={tenis6} alt="Tênis 6" />
-            <h3>FORUM 84 LOW</h3>
-            <p>R$ 579,99</p>
-            <p>Adicionar ao Carrinho</p>
+        <div className="comprar-container">
+            <img src={tenis6} alt="Tênis 6" className="tenis-img"/>
+            <h3 className="tenis-nome">FORUM 84 LOW</h3>
+            <p className="tenis-preco">R$ 579,99</p>
+            <button className="btn-comprar">Adicionar ao Carrinho</button>
         </div>
     );
 }
