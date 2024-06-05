852266218edafff5f85f9eacd0e692eaae8f0ed5
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 03:40:45 -0300

    Update ComprarTenis8.jsx

--- frontend/src/pages/ComprarTenis8.jsx
+++ frontend/src/pages/ComprarTenis8.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import tenis8 from '../images/tenis8.jpg'; 
+import './ComprarTenis.css'; 
 
 
 const ComprarTenis8 = () => {
     return (
-        <div>
-            <img src={tenis8} alt="Tênis 8" />
-            <h3>ADI2000</h3>
-            <p>R$ 799,99</p>
-            <p>Adicionar ao Carrinho</p>
+        <div className="comprar-container">
+            <img src={tenis8} alt="Tênis 8" className="tenis-img"/>
+            <h3 className="tenis-nome">ADI2000</h3>
+            <p className="tenis-preco">R$ 799,99</p>
+            <button className="btn-comprar">Adicionar ao Carrinho</button>
         </div>
     );
 }
