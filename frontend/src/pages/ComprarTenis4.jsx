ae49067309a0ae7b648c5f8e76c9477fb1595094
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 02:42:17 -0300

    Create ComprarTenis4.jsx

---
+++ frontend/src/pages/ComprarTenis4.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ComprarTenis1 = () => {
+    return (
+        <div>
+            <h3>JORDAN 1 MID</h3>
+            <p>R$ 839,99</p>
+            <p>Adicionar ao Carrinho</p>
+        </div>
+    );
+}
+
+export default ComprarTenis1;
