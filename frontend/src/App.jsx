949f3cb2b3057720780d491a31d84e96b7c8961b
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 12:56:16 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,9 @@ import ComprarTenis6 from './pages/ComprarTenis6';
 import ComprarTenis7 from './pages/ComprarTenis7';
 import ComprarTenis8 from './pages/ComprarTenis8';
 import ComprarTenis9 from './pages/ComprarTenis9';
+import QuemSomos from './pages/QuemSomos';
+import Ajuda from './pages/Ajuda';
+import Suporte from './pages/Suporte';
 import './App.css'
 
 function App() {
@@ -38,6 +41,9 @@ function App() {
             <Route path="/comprar/tenis7" element={<ComprarTenis7 />} />
             <Route path="/comprar/tenis8" element={<ComprarTenis8 />} />
             <Route path="/comprar/tenis9" element={<ComprarTenis9 />} />
+            <Route path="/quem/somos" element={<QuemSomos />} />
+            <Route path='/ajuda' element={<Ajuda />} />
+            <Route path='/suporte' element={<Suporte />} />
           </Routes>
 
         </main>
