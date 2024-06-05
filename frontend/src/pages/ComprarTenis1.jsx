3048ee83bbb2ba9cd0169ea5a7ae564230137a61
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 02:42:13 -0300

    Create ComprarTenis1.jsx

---
+++ frontend/src/pages/ComprarTenis1.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ComprarTenis1 = () => {
+    return (
+        <div>
+            <h3>DUNK LOW OG</h3>
+            <p>R$ 589,99</p>
+            <p>Adicionar ao Carrinho</p>
+        </div>
+    );
+}
+
+export default ComprarTenis1;
