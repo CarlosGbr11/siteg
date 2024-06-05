04052bac120b5656b4fd11fe54010319a4386ceb
CarlosGbr11 <carlos157759@gmail.com>
2024-06-05 00:31:40 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './pages/Footer';
 import Header from './pages/Header';
 import Inicio from './components/Inicio';
-import Vestuario from './components/Vestuario';
 import Nike from './components/Nike';
 import Adidas from './components/Adidas';
 import './App.css'
@@ -19,7 +18,6 @@ function App() {
         <main>
           <Routes>
             <Route path='/' element={<Inicio />} />
-            <Route path='/Vestuario' element={<Vestuario />} />
             <Route path='/Nike' element={<Nike />} />
             <Route path='/Adidas' element={<Adidas />} />
           </Routes>
