cb0a75d3391b15871fb9e1bdd1a1afdac0f578ee
CarlosGbr11 <carlos157759@gmail.com>
2024-06-06 15:47:29 -0300

    Update feedController.js

--- backend/controller/feedController.js
+++ backend/controller/feedController.js
@@ -1,49 +1,49 @@
-import { create, read, update, deletePes } from '../model/pessoaModel.js';
+import { create, read, update, deleteFeed } from '../model/feedModel.js';
 
-export async function createPessoa(req, res){
-    const { nome, dataNascimento, genero, cpf, telefone } = req.body;
-    console.log('Dados recebidos do frontend:', { nome, dataNascimento, genero, cpf, telefone });
+export async function createFeed(req, res){
+    const { texto } = req.body;
+    console.log('Dados recebidos do frontend:', { texto });
 
-    create(nome, dataNascimento, genero, cpf, telefone, (err, result) => {
+    create(texto, (err, result) => {
         if (err) {
             res.status(500).json({ error: err.message });
             return;
         }
-        res.status(201).json({ mensagem: 'Pessoa criada com sucesso' });
+        res.status(201).json({ mensagem: 'Feedback criado com sucesso' });
     });
 }
 
-export async function getAllPessoas(req, res) {
-    read((err, pessoas) => {
+export async function getAllFeed(req, res) {
+    read((err, feeds) => {
         if (err) {
             read.status(500).json({ error: err.message });
             return;
         }
-        res.json(pessoas);
+        res.json(feeds);
     });
 }
 
-export async function updatePessoa(req, res) {
+export async function updateFeed(req, res) {
     const { id } = req.params;
     console.log('Dados recebidops do  frontend:', {id});
-    const { nome, dataNascimento, genero, cpf, telefone} = req.body;
-    update(nome, dataNascimento, genero, cpf, telefone, id, (err, result) => {
+    const { texto } = req.body;
+    update(texto, id, (err, result) => {
         if (err) {
             res.status(500).json({ error: err.message });
             return;
         }
-        res.send('Pessoa atualizada com sucesso');
+        res.send('Feed atualizado com sucesso');
     });
 }
 
-export async function deletePessoa(req, res) {
+export async function deleteFeed(req, res) {
     const { id } = req.params;
     console.log('delete recebidos do frontend:', {id});
-    deletePes(id, (err, result) => {
+    deleteFeed(id, (err, result) => {
         if (err) {
             res.status(500).json({ error: err.message });
             return;
         }
-        res.send('Pessoa excluida com sucesso');
+        res.send('Feed excluido com sucesso');
     });
 }
