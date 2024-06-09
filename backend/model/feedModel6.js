31a0051cdca36be70956d04862348276c0b201e0
CarlosGbr11 <carlos157759@gmail.com>
2024-06-09 12:28:21 -0300

    Create feedModel6.js

---
+++ backend/model/feedModel6.js
@@ -0,0 +1,14 @@
+import connection from '../config/Conecta.js';
+
+export function read(callback){
+    connection.query('SELECT * from feedback6', callback);
+}
+export function create(nome6, texto6, callback){
+    connection.query('INSERT INTO feedback6 (nome6, texto6) VALUES (?, ?)', [nome6,texto6], callback);
+}
+export function update(nome6, texto6, id, callback) {
+    connection.query("UPDATE feedback6 SET nome6 = ?, texto6 = ?, ativo_feedback = 1 WHERE idfeedback = ?", [nome6, texto6, id], callback);
+}
+export function deleteF6(id, callback) {
+    connection.query('DELETE from feedback6 where idfeedback = ?', [id], callback);
+}
