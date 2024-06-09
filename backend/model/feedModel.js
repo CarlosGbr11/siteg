5cc8967ee27aaba4c8c7e0be14907011f5694024
CarlosGbr11 <carlos157759@gmail.com>
2024-06-09 02:43:41 -0300

    Update feedModel.js

--- backend/model/feedModel.js
+++ backend/model/feedModel.js
@@ -3,11 +3,11 @@ import connection from '../config/Conecta.js';
 export function read(callback){
     connection.query('SELECT * from feedback', callback);
 }
-export function create(texto, callback){
-    connection.query('INSERT INTO feedback (texto) VALUES (?)', [texto], callback);
+export function create(nome, texto, callback){
+    connection.query('INSERT INTO feedback (nome, texto) VALUES (?, ?)', [nome,texto], callback);
 }
-export function update(texto, id, callback) {
-    connection.query("UPDATE feedback SET texto = ?, ativo_feedback = 1 WHERE idfeedback = ?", [texto, id], callback);
+export function update(nome, texto, id, callback) {
+    connection.query("UPDATE feedback SET nome = ?, texto = ?, ativo_feedback = 1 WHERE idfeedback = ?", [nome, texto, id], callback);
 }
 export function deleteF(id, callback) {
     connection.query('DELETE from feedback where idfeedback = ?', [id], callback);
