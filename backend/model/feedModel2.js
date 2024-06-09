c7c0d1f2c4a2baa22aef15fe02abf9dc27da044b
CarlosGbr11 <carlos157759@gmail.com>
2024-06-09 04:31:40 -0300

    Create feedModel2.js

---
+++ backend/model/feedModel2.js
@@ -0,0 +1,14 @@
+import connection from '../config/Conecta.js';
+
+export function read(callback){
+    connection.query('SELECT * from feedback2', callback);
+}
+export function create(nome2, texto2, callback){
+    connection.query('INSERT INTO feedback2 (nome2, texto2) VALUES (?, ?)', [nome2,texto2], callback);
+}
+export function update(nome2, texto2, id, callback) {
+    connection.query("UPDATE feedback2 SET nome2 = ?, texto2 = ?, ativo_feedback = 1 WHERE idfeedback = ?", [nome2, texto2, id], callback);
+}
+export function deleteF2(id, callback) {
+    connection.query('DELETE from feedback2 where idfeedback = ?', [id], callback);
+}
