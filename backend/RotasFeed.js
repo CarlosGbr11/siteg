23846b928a9ea931a46b97415471d8ae585148e7
CarlosGbr11 <carlos157759@gmail.com>
2024-06-09 04:31:44 -0300

    Update RotasFeed.js

--- backend/RotasFeed.js
+++ backend/RotasFeed.js
@@ -2,6 +2,8 @@ import express from 'express';
 const app = express();
 import cors from 'cors';
 import { getAllFeed, createFeed, updateFeed, deleteFeed } from "./controller/feedController.js";
+import { getAllFeed2, createFeed2, updateFeed2, deleteFeed2 } from "./controller/feedController2.js";
+import { getAllFeed3, createFeed3, updateFeed3, deleteFeed3 } from "./controller/feedController3.js";
 
 
 app.use(express.json());
@@ -11,6 +13,16 @@ app.get('/feed', getAllFeed);
 app.post('/feed', createFeed);
 app.put('/feed/:id', updateFeed);
 app.delete('/feed/:id', deleteFeed);
+
+app.get('/feed2', getAllFeed2);
+app.post('/feed2', createFeed2);
+app.put('/feed2/:id', updateFeed2);
+app.delete('/feed2/:id', deleteFeed2);
+
+app.get('/feed3', getAllFeed3);
+app.post('/feed3', createFeed3);
+app.put('/feed3/:id', updateFeed3);
+app.delete('/feed3/:id', deleteFeed3);
 
 app.listen(3000, () => {
     console.log(`Servidor rodando com sucesso na porta 3000`);
