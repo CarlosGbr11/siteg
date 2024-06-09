e06c24face84bf33d41853c8b8536c9cd30760cb
CarlosGbr11 <carlos157759@gmail.com>
2024-06-09 14:12:36 -0300

    Update feedModel.js

--- backend/model/feedModel.js
+++ backend/model/feedModel.js
@@ -7,7 +7,7 @@ export function create(nome, texto, callback){
     connection.query('INSERT INTO feedback1 (nome, texto) VALUES (?, ?)', [nome,texto], callback);
 }
 export function update(nome, texto, id, callback) {
-    connection.query("UPDATE feedback1 SET nome = ?, texto = ?, ativo_feedback = 1 WHERE idfeedback = ?", [nome, texto, id], callback);
+    connection.query("UPDATE feedback1 SET nome = ?, texto = ?  WHERE idfeedback = ?", [nome, texto, id], callback);
 }
 export function deleteF(id, callback) {
     connection.query('DELETE from feedback1 where idfeedback = ?', [id], callback);
