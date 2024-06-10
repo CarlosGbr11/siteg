bf5238f369e119bc8103bb9c2735e2cbb18f487c
CarlosGbr11 <carlos157759@gmail.com>
2024-06-09 21:31:39 -0300

    Update ComprarTenis1.jsx

--- frontend/src/pages/ComprarTenis1.jsx
+++ frontend/src/pages/ComprarTenis1.jsx
@@ -109,7 +109,7 @@ function ComprarTenis1() {
           Comentário:
           <input type="text" name="texto" value={formValores.texto} onChange={handleChange} className="comentario-input" />
         </label>
-        <button type='submit' className="btn-comprar">{editMode ? 'Atualizar comentário' : 'Adicionar comentário'}</button>
+        <button type='submit' className="btn-comprar">{editMode ? 'Responder comentário' : 'Adicionar comentário'}</button>
       </form>
       
       <div className="read-container">
