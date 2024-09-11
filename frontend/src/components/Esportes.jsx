956ae06ec2aa7bce3ffd858875cd50be60697b32
CarlosGbr11 <carlos157759@gmail.com>
2024-09-11 17:50:43 -0300

    Update Esportes.jsx

--- frontend/src/components/Esportes.jsx
+++ frontend/src/components/Esportes.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import './Esportes.css'
+import Anuncios from "./Anuncios";
 
 const Esportes = () => {
     return (
 
-        <div>
-            <p>Esportes</p>
-        </div>
+        <div className='esportes'>
+        <h1>ESPORTES</h1>
+        <p>Descubra nossa linha de roupas para treino, projetadas para maximizar seu desempenho em qualquer atividade física. Cada peça é confeccionada com materiais respiráveis e tecnológicos que garantem conforto e liberdade de movimento. Seja para musculação, cardio, ou qualquer outro tipo de exercício, nossas roupas foram pensadas para te acompanhar na busca pelos seus objetivos.</p>
+        <Anuncios />
+    </div>
     )
 }
 export default Esportes;
