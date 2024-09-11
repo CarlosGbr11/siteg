8ab237ff6835241925a372941c2ea19cc235bad3
CarlosGbr11 <carlos157759@gmail.com>
2024-09-11 18:11:10 -0300

    Update Infantil.jsx

--- frontend/src/components/Infantil.jsx
+++ frontend/src/components/Infantil.jsx
@@ -1,5 +1,5 @@
 import './Infantil.css'
-import Anuncios from "./Anuncios";
+import Anuncios4 from "./Anuncios4";
 
 const Infantil = () => {
     return (
@@ -7,7 +7,7 @@ const Infantil = () => {
         <div className='infantil'>
         <h1>Infantil</h1>
         <p>Descubra nossa encantadora coleção de roupas infantis, onde conforto e diversão andam de mãos dadas. Cada peça foi pensada para acompanhar o dia a dia das crianças com estilo e qualidade. Nossas roupas combinam cores vibrantes, estampas divertidas e tecidos macios, perfeitos para explorar o mundo com liberdade e alegria</p>
-        <Anuncios />
+        <Anuncios4 />
     </div>
     )
 }
