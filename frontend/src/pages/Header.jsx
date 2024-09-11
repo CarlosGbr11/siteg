f6958833dc41a0dcd5787992faaa8a27ee22ee8b
CarlosGbr11 <carlos157759@gmail.com>
2024-09-11 16:51:13 -0300

    Update Header.jsx

--- frontend/src/pages/Header.jsx
+++ frontend/src/pages/Header.jsx
@@ -6,9 +6,13 @@ const Header = () => {
     return (
 
         <div className="container-header">
-            <h1>HYPE</h1>
+            <Link to='/'><h1>HYPE</h1></Link>
             <div className="links-header">
-                <Link to='/'><span>Ínicio</span></Link>
+                <Link to='/calçados'><span>CALÇADOS</span></Link>
+                <Link to='/mulher'><span>MULHER</span></Link>
+                <Link to='/homem'><span>HOMEM</span></Link>
+                <Link to='/infantil'><span>INFANTIL</span></Link>
+                <Link to='/esportes'><span>ESPORTES</span></Link>
             </div>
         </div>
     )
