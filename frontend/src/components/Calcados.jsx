35bde4bb72ce5d071667c97b56fedbae69fa1e9b
CarlosGbr11 <carlos157759@gmail.com>
2024-09-11 17:41:21 -0300

    Create Calcados.jsx

---
+++ frontend/src/components/Calcados.jsx
@@ -0,0 +1,15 @@
+import './Calcados.css'
+import Anuncios from "./Anuncios";
+
+const Calcados = () => {
+    return (
+
+        <div className='calcados'>
+            <h1>CALÇADOS</h1>
+            <p>Aqui você encontra uma linda coleção de calçados. Escolha o seu e eleve o seu estilo com as últimas tendências. Cada par foi selecionado com cuidado para garantir conforto e elegância. Encontre as melhores marcas que combinam com o seu estilo único, e descubra o prazer de calçar qualidade e design. Seja para o dia a dia ou para uma ocasião especial, temos o par perfeito para você.</p>
+            <Anuncios />
+        </div>
+       
+    )
+}
+export default Calcados;
