f27e2222bc73badf21b313e1e56fb58fe5f56c78
CarlosGbr11 <carlos157759@gmail.com>
2024-09-11 17:50:52 -0300

    Update Mulher.jsx

--- frontend/src/components/Mulher.jsx
+++ frontend/src/components/Mulher.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import './Mulher.css'
+import Anuncios from "./Anuncios";
 
 const Mulher = () => {
     return (
 
-        <div>
-            <p>Mulher</p>
-        </div>
+        <div className='mulher'>
+        <h1>MULHER</h1>
+        <p>Aqui, você descobre uma seleção exclusiva de roupas femininas que realçam o seu estilo com as últimas tendências. Cada peça é escolhida com atenção aos detalhes, priorizando conforto e elegância. Explore as marcas que melhor refletem a sua personalidade e sinta a diferença de vestir qualidade e design.</p>
+        <Anuncios />
+    </div>
     )
 }
 export default Mulher;
