b3e07ca4221a9f31067f9719e91767822725c393
CarlosGbr11 <carlos157759@gmail.com>
2024-09-11 18:11:09 -0300

    Update Homem.jsx

--- frontend/src/components/Homem.jsx
+++ frontend/src/components/Homem.jsx
@@ -1,5 +1,5 @@
 import './Homem.css'
-import Anuncios from "./Anuncios";
+import Anuncios3 from "./Anuncios3";
 
 const Homem = () => {
     return (
@@ -7,7 +7,7 @@ const Homem = () => {
         <div className='homem'>
         <h1>HOMEM</h1>
         <p>Aqui, você encontra uma seleção exclusiva de roupas masculinas que elevam o seu estilo com as últimas tendências. Cada peça é cuidadosamente escolhida para garantir conforto e sofisticação. Explore as marcas que combinam com sua personalidade e experimente a diferença de vestir qualidade e design.</p>
-        <Anuncios />
+        <Anuncios3 />
     </div>
     )
 }
