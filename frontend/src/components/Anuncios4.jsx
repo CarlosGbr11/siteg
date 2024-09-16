5edf47e096cb10e1c35666f722fe7264b3970fd7
CarlosGbr11 <carlos157759@gmail.com>
2024-09-15 21:37:39 -0300

    Update Anuncios4.jsx

--- frontend/src/components/Anuncios4.jsx
+++ frontend/src/components/Anuncios4.jsx
@@ -1,66 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import './Anuncios.css'
-import tenis1 from '../images/tenis1.jpg'; 
-import tenis2 from '../images/tenis2.jpg'; 
-import tenis3 from '../images/tenis3.jpg'; 
-import tenis4 from '../images/tenis4.jpg'; 
-import tenis5 from '../images/tenis5.jpg'; 
-import tenis6 from '../images/tenis6.jpg'; 
-import tenis7 from '../images/tenis7.jpg'; 
-import tenis8 from '../images/tenis8.jpg'; 
-import tenis9 from '../images/tenis9.jpg'; 
+import inf1 from '../images/inf1.avif'; 
+import inf2 from '../images/inf2.avif'; 
+import inf3 from '../images/inf3.avif'; 
+import inf4 from '../images/inf4.avif'; 
+import inf5 from '../images/inf5.avif'; 
+import inf6 from '../images/inf6.avif'; 
+import inf7 from '../images/inf7.avif'; 
+import inf8 from '../images/inf8.avif'; 
+import inf9 from '../images/inf9.avif'; 
+import inf10 from '../images/inf10.avif'; 
+import inf11 from '../images/inf11.avif'; 
+import inf12 from '../images/inf12.avif'; 
+import inf13 from '../images/inf13.avif'; 
+import inf14 from '../images/inf14.avif'; 
+import inf15 from '../images/inf15.avif'; 
+import inf16 from '../images/inf16.avif'; 
+import inf17 from '../images/inf17.avif'; 
+import inf18 from '../images/inf18.avif'; 
 
 
-function Anuncios4() {
+function Anuncios3() {
+    const anuncios = [
+        { id: 1, img: inf1, nome: "Camiseta Sports Masculina", tipo: "casual", preco: "R$ 79,99" },
+        { id: 2, img: inf2, nome: "Camiseta Patch Masculina", tipo: "casual", preco: "R$ 79,99" },
+        { id: 3, img: inf3, nome: "Camiseta FIT Masculina", tipo: "casual", preco: "R$ 79,99" },
+        { id: 4, img: inf4, nome: "Camiseta HTG Masculina", tipo: "casual", preco: "R$ 79,99" },
+        { id: 5, img: inf5, nome: "Camiseta Mx90 Masculina", tipo: "casual", preco: "R$ 79,99" },
+        { id: 6, img: inf6, nome: "Camiseta ACG Masculina", tipo: "casual", preco: "R$ 79,99" },
+        { id: 7, img: inf7, nome: "Camiseta DOT Masculina", tipo: "casual", preco: "R$ 79,99" },
+        { id: 8, img: inf8, nome: "Camiseta EST Masculina", tipo: "casual", preco: "R$ 79,99" },
+        { id: 9, img: inf9, nome: "Camiseta DRI Masculina", tipo: "casual", preco: "R$ 79,99" },
+        { id: 10, img: inf10, nome: "Camiseta Club Masculina", tipo: "casual", preco: "R$ 79,99" },
+        { id: 11, img: inf11, nome: "Camiseta Trail Masculina", tipo: "casual", preco: "R$ 79,99" },
+        { id: 12, img: inf12, nome: "Camiseta LeBr Masculina", tipo: "casual", preco: "R$ 79,99" },
+        { id: 13, img: inf13, nome: "Camiseta Flight Masculina", tipo: "casual", preco: "R$ 79,99" },
+        { id: 14, img: inf14, nome: "Camiseta Jrda Masculina", tipo: "casual", preco: "R$ 79,99" },
+        { id: 15, img: inf15, nome: "Camiseta Sport Masculina", tipo: "casual", preco: "R$ 79,99" },
+        { id: 16, img: inf16, nome: "Camiseta Just Masculina", tipo: "casual", preco: "R$ 79,99" },
+        { id: 17, img: inf17, nome: "Camiseta Park Masculina", tipo: "casual", preco: "R$ 79,99" },
+        { id: 18, img: inf18, nome: "Camiseta ADV Masculina", tipo: "corrida", preco: "R$ 79,99" },
+        
+    ];
+
+    const [paginaAtual, setPaginaAtual] = useState(1);
+    const itensPorPagina = 9; 
+
+    const indiceUltimoItem = paginaAtual * itensPorPagina;
+    const indicePrimeiroItem = indiceUltimoItem - itensPorPagina;
+    const anunciosAtuais = anuncios.slice(indicePrimeiroItem, indiceUltimoItem);
+
+    const proximaPagina = () => {
+        if (paginaAtual < Math.ceil(anuncios.length / itensPorPagina)) {
+            setPaginaAtual(paginaAtual + 1);
+        }
+    };
+
+    const paginaAnterior = () => {
+        if (paginaAtual > 1) {
+            setPaginaAtual(paginaAtual - 1);
+        }
+    };
+
     return (
-        <div className="anuncios-container">
-            <a href="/comprar/tenis1" className="anuncio">
-                <img src={tenis1} alt="Tênis 1" />
-                <h3>DUNK LOW OG</h3>
-                <p>R$ 589,99</p>
-            </a>
-            <a href="/comprar/tenis2" className="anuncio">
-                <img src={tenis2} alt="Tênis 2" />
-                <h3>DUNK LOW RETRO</h3>
-                <p>R$ 639,99</p>
-            </a>
-            <a href="/comprar/tenis3" className="anuncio">
-                <img src={tenis3} alt="Tênis 3" />
-                <h3>AIR FORCE 1 ´07 LV8</h3>
-                <p>R$ 679,99</p>
-            </a>
-            <a href="/comprar/tenis4" className="anuncio">
-                <img src={tenis4} alt="Tênis 4" />
-                <h3>JORDAN 1 MID</h3>
-                <p>R$ 839,99</p>
-            </a>
-            <a href="/comprar/tenis5" className="anuncio">
-                <img src={tenis5} alt="Tênis 5" />
-                <h3>STAN SMITH</h3>
-                <p>R$ 339,99</p>
-            </a>
-            <a href="/comprar/tenis6" className="anuncio">
-                <img src={tenis6} alt="Tênis 6" />
-                <h3>FORUM 84 LOW</h3>
-                <p>R$ 579,99</p>
-            </a>
-            <a href="/comprar/tenis7" className="anuncio">
-                <img src={tenis7} alt="Tênis 7" />
-                <h3>HANDBALL SPEZIAL</h3>
-                <p>R$ 399,99</p>
-            </a>
-            <a href="/comprar/tenis8" className="anuncio">
-                <img src={tenis8} alt="Tênis 8" />
-                <h3>ADI2000</h3>
-                <p>R$ 799,99</p>
-            </a>
-            <a href="/comprar/tenis9" className="anuncio">
-                <img src={tenis9} alt="Tênis 9" />
-                <h3>SUPERSTAR XLG</h3>
-                <p>R$ 399,99</p>
-            </a>
+        <div>
+            <div className="anuncios-container">
+                {anunciosAtuais.map(anuncio => (
+                    <a href={`/comprar/tenis${anuncio.id}`} key={anuncio.id} className="anuncio">
+                        <img src={anuncio.img} alt={anuncio.nome} />
+                        <h3>{anuncio.nome}</h3>
+                        <h4>{anuncio.tipo}</h4>
+                        <p>{anuncio.preco}</p>
+                    </a>
+                ))}
+            </div>
+            
+            <div className="paginacao">
+                <button onClick={paginaAnterior} disabled={paginaAtual === 1}>
+                    Anterior
+                </button>
+                <span>Página {paginaAtual} de {Math.ceil(anuncios.length / itensPorPagina)}</span>
+                <button onClick={proximaPagina} disabled={paginaAtual === Math.ceil(anuncios.length / itensPorPagina)}>
+                    Próximo
+                </button>
+            </div>
         </div>
     );
 }
 
-export default Anuncios4;
+export default Anuncios3;
