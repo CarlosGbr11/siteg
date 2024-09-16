1be539706a1b1c59684ee5a2374acb58a1650db0
CarlosGbr11 <carlos157759@gmail.com>
2024-09-15 22:05:36 -0300

    Update ComprarFem.jsx

--- frontend/src/pages/ComprarFem.jsx
+++ frontend/src/pages/ComprarFem.jsx
@@ -1,12 +1,10 @@
 import feminino1 from '../images/feminino1.avif';
-import './ComprarTenis.css';
-import './Comentarios.css';
 
 function ComprarFem() {
   return (
     <div className="comprar-container">
       <img src={feminino1} alt="fem1" className="tenis-img" />
-      <h3 className="tenis-nome">DUNK LOW OG</h3>
+      <h3 className="tenis-nome">D</h3>
       <p className="tenis-preco">R$ 589,99</p>
     </div>
   );
