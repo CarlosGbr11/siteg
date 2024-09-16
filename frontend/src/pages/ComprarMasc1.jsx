0d3234909ff31b940ba9125f72926727c6a45963
CarlosGbr11 <carlos157759@gmail.com>
2024-09-15 22:43:16 -0300

    Update ComprarMasc1.jsx

--- frontend/src/pages/ComprarMasc1.jsx
+++ frontend/src/pages/ComprarMasc1.jsx
@@ -4,9 +4,9 @@ import './Comprar.css';
 function ComprarMasc1() {
   return (
     <div className="comprar-container">
-      <img src={masc1} alt="fem1" className="tenis-img" />
-      <h3 className="tenis-nome">D</h3>
-      <p className="tenis-preco">R$ 589,99</p>
+      <img src={masc1} alt="masc1" className="prod-img" />
+      <h3 className="prod-nome">Camiseta Sports Masculina</h3>
+      <p className="prod-preco">R$ 79,99</p>
     </div>
   );
 }
