9601e21ec603b4d19daf9961a59f7e90d62b0a3c
CarlosGbr11 <carlos157759@gmail.com>
2024-09-26 10:46:37 -0300

    Update Homem.jsx

--- frontend/src/components/Homem.jsx
+++ frontend/src/components/Homem.jsx
@@ -1,14 +1,25 @@
-import './Homem.css'
+import './Homem.css';
 import Anuncios3 from "./Anuncios3";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const Homem = () => {
     return (
-
         <div className='homem'>
-        <h1>HOMEM</h1>
-        <p>Aqui, você encontra uma seleção exclusiva de roupas masculinas que elevam o seu estilo com as últimas tendências. Cada peça é cuidadosamente escolhida para garantir conforto e sofisticação. Explore as marcas que combinam com sua personalidade e experimente a diferença de vestir qualidade e design.</p>
-        <Anuncios3 />
-    </div>
-    )
-}
+            <div className="header">
+                <h1>HOMEM</h1>
+                <div className="cart-icon">
+                    <FontAwesomeIcon icon={faShoppingCart} size="2x" />
+                </div>
+            </div>
+            <p>
+                Aqui, você encontra uma seleção exclusiva de roupas masculinas que elevam o seu estilo com as últimas tendências. 
+                Cada peça é cuidadosamente escolhida para garantir conforto e sofisticação. Explore as marcas que combinam com sua personalidade 
+                e experimente a diferença de vestir qualidade e design.
+            </p>
+            <Anuncios3 />
+        </div>
+    );
+};
+
 export default Homem;
