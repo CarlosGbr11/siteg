81344a7c17a86d83ac943826cf2f1ca71278f0ad
CarlosGbr11 <carlos157759@gmail.com>
2024-09-26 10:46:42 -0300

    Update Mulher.jsx

--- frontend/src/components/Mulher.jsx
+++ frontend/src/components/Mulher.jsx
@@ -1,14 +1,25 @@
-import './Mulher.css'
+import './Mulher.css';
 import Anuncios2 from "./Anuncios2";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const Mulher = () => {
     return (
-
         <div className='mulher'>
-        <h1>MULHER</h1>
-        <p>Aqui, você descobre uma seleção exclusiva de roupas femininas que realçam o seu estilo com as últimas tendências. Cada peça é escolhida com atenção aos detalhes, priorizando conforto e elegância. Explore as marcas que melhor refletem a sua personalidade e sinta a diferença de vestir qualidade e design.</p>
-        <Anuncios2 />
-    </div>
-    )
-}
+            <div className="header">
+                <h1>MULHER</h1>
+                <div className="cart-icon">
+                    <FontAwesomeIcon icon={faShoppingCart} size="2x" />
+                </div>
+            </div>
+            <p>
+                Aqui, você descobre uma seleção exclusiva de roupas femininas que realçam o seu estilo com as últimas tendências. 
+                Cada peça é escolhida com atenção aos detalhes, priorizando conforto e elegância. Explore as marcas que melhor 
+                refletem a sua personalidade e sinta a diferença de vestir qualidade e design.
+            </p>
+            <Anuncios2 />
+        </div>
+    );
+};
+
 export default Mulher;
